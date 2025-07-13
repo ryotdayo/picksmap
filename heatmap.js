ac3e0d96d958df6cd8a96f7d6ffe468253364783
ryotdayo <ayako.tsuchida.meme3@gmail.com>
2025-07-13 22:49:53 +0900

    Update heatmap.js

--- heatmap.js
+++ heatmap.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const width = 1000;
-  const height = 600;
+  const height = 1600;
 
   const treeMap = d3.select('#tree-map')
                     .append('svg')
