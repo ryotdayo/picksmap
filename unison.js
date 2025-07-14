221da5dd743d2c35773d2312ea0191ac9691739c
ryotdayo <ayako.tsuchida.meme3@gmail.com>
2025-07-14 11:44:37 +0900

    Update unison.js

--- unison.js
+++ unison.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
- // const width = 100%;
-//  const height = 70vh;
+  const width = 1000;
+  const height = 600;
 
  // const treeMap = d3.select('#tree-map')
             //        .append('svg')
