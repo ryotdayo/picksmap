1e83b6d288afe80ca64f3d04ebee220090376710
ryotdayo <ayako.tsuchida.meme3@gmail.com>
2025-07-14 13:51:29 +0900

    Update unison.js

--- unison.js
+++ unison.js
@@ -147,7 +147,11 @@ const paddingTop = 10;  // 上に10px（最初の行の高さ）
             .text((d) => d)
             .attr('x', 3)
          .attr('y', (d, i) => 13 + i * 11)
-           .style('font-size', 15 + 'px')
+         //  .style('font-size', 15 + 'px')
+       .style('font-size', (d) => {
+    if (d === 'OK') return '20px';  // ← ここで条件分岐
+    else return '15px';
+  })
           .style('fill', 'white')
             .style('font-weight', 'bold');  
 
